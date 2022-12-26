c8ca73d6d14014d31d68a7dcbdb36546ffd96637
CONSULitAS <CONSULitAS@users.noreply.github.com>
2022-12-27 00:02:23 +0100

    Update docker-entrypoint.sh
    - deal with ENV variables (shellcheck)
    - build custom GROUP
    - recognize "lost+found" folder (bugfix - my mount point has a offline.txt -> fail)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ -z "${WEBDRIVE_USERNAME}" ]; then
     echo "No username specified, is this on purpose?"
 fi
 if [ -n "${WEBDRIVE_PASSWORD_FILE}" ]; then
-    WEBDRIVE_PASSWORD=$(read ${WEBDRIVE_PASSWORD_FILE})
+    WEBDRIVE_PASSWORD=$(cat "$WEBDRIVE_PASSWORD_FILE")
 fi
 if [ -z "${WEBDRIVE_PASSWORD}" ]; then
     echo "No password specified, is this on purpose?"
@@ -33,30 +33,40 @@ if [ -n "$(env | grep "DAVFS2_")" ]; then
         if [ -n "$(echo "$VAR" | grep -E '^DAVFS2_')" ]; then
             OPT_NAME=$(echo "$VAR" | sed -r "s/DAVFS2_([^=]*)=.*/\1/g" | tr '[:upper:]' '[:lower:]')
             VAR_FULL_NAME=$(echo "$VAR" | sed -r "s/([^=]*)=.*/\1/g")
-            VAL=$(eval echo \$$VAR_FULL_NAME)
+            VAL=$(eval echo "\$$VAR_FULL_NAME")
             echo "$OPT_NAME $VAL" >> /etc/davfs2/davfs2.conf
         fi
     done
 fi
 
 # Create destination directory if it does not exist.
-if [ ! -d $DEST ]; then
-    mkdir -p $DEST
+if [ ! -d "$DEST" ]; then
+    mkdir -p "$DEST"
+fi
+
+# Deal with new group
+if [ "$GROUP" = "users" ]; then
+    echo "Group users should already exist."
+else
+    # add any other group
+    addgroup "$GROUP"
 fi
 
 # Deal with ownership
-if [ $OWNER -gt 0 ]; then
-    adduser webdrive -u $OWNER -D -G users
-    chown webdrive $DEST
+if [ "$OWNER" -gt 0 ]; then
+    adduser webdrive -u "$OWNER" -D -G "$GROUP"
+    chown webdrive "$DEST"
 fi
 
 # Mount and verify that something is present. davfs2 always creates a lost+found
 # sub-directory, so we can use the presence of some file/dir as a marker to
 # detect that mounting was a success. Execute the command on success.
-mount -t davfs $WEBDRIVE_URL $DEST -o uid=$OWNER,gid=users,dir_mode=755,file_mode=755
-if [ -n "$(ls -1A $DEST)" ]; then
+mount -t davfs "$WEBDRIVE_URL" "$DEST" -o uid="$OWNER",gid="$GROUP",dir_mode=750,file_mode=750
+if [ -d "$DEST/lost+found" ]; then
     echo "Mounted $WEBDRIVE_URL onto $DEST"
     exec "$@"
 else
     echo "Nothing found in $DEST, giving up!"
 fi
+
+# ***** EOF *****
